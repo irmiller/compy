0e5ce190298a52a4bf896fb9ee5abf3fe39e5b63
irmiller <63781021+irmiller@users.noreply.github.com>
2020-04-17 12:19:20 +0200

    Add files via upload

--- transcoder/util.go
+++ transcoder/util.go
@@ -7,7 +7,7 @@ import (
 )
 
 func SupportsWebP(headers http.Header) bool {
-	log.Printf("cat")
+	log.Printf(logtest)
 	for _, v := range strings.Split(headers.Get("Accept"), ",") {
 		if strings.SplitN(v, ";", 2)[0] == "image/webp" {
 			return true
