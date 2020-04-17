db5d94bae7be0e2fd8c5f13d46a5bc5275c97f24
irmiller <63781021+irmiller@users.noreply.github.com>
2020-04-17 12:25:21 +0200

    Add files via upload

--- proxy/mitmconn.go
+++ proxy/mitmconn.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 	"time"
 )
-f
+
 type mitmConn struct {
 	w          FlushWriter
 	r          io.Reader
--- transcoder/util.go
+++ transcoder/util.go
@@ -3,11 +3,9 @@ package transcoder
 import (
 	"net/http"
 	"strings"
-	"log"
 )
 
 func SupportsWebP(headers http.Header) bool {
-	log.Printf(logtest)
 	for _, v := range strings.Split(headers.Get("Accept"), ",") {
 		if strings.SplitN(v, ";", 2)[0] == "image/webp" {
 			return true
