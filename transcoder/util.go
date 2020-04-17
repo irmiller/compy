b105a5a2867273a2e20884a9122e0d19670a07b1
irmiller <63781021+irmiller@users.noreply.github.com>
2020-04-17 11:48:49 +0200

    Update util.go

--- transcoder/util.go
+++ transcoder/util.go
@@ -3,9 +3,11 @@ package transcoder
 import (
 	"net/http"
 	"strings"
+	"fmt"
 )
 
 func SupportsWebP(headers http.Header) bool {
+	fmt.Println("cat")
 	for _, v := range strings.Split(headers.Get("Accept"), ",") {
 		if strings.SplitN(v, ";", 2)[0] == "image/webp" {
 			return true
