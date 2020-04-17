98db2d8fe77ccef98e395f13606ea30d3185e902
irmiller <63781021+irmiller@users.noreply.github.com>
2020-04-17 13:07:13 +0200

    Update compy.go

--- compy.go
+++ compy.go
@@ -56,7 +56,7 @@ func main() {
 	}
 
 	if *jpeg != 0 {
-		p.AddTranscoder("image/jpeg", &tc.Jpeg{})
+		p.AddTranscoder("image/jpeg", tc.NewJpeg(*jpeg))
 	}
 	if *gif {
 		p.AddTranscoder("image/gif", &tc.Gif{})
