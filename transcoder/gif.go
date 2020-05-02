68bdcb78f58f654de00d34230b3e599f94afa701
irmiller <63781021+irmiller@users.noreply.github.com>
2020-05-02 09:29:59 +0200

    Update gif.go

--- transcoder/gif.go
+++ transcoder/gif.go
@@ -12,33 +12,17 @@ import (
 type Gif struct{}
 
 func (t *Gif) Transcode(w *proxy.ResponseWriter, r *proxy.ResponseReader, headers http.Header) error {
-	//img, err := gif.Decode(r)
 	gifBin, _  := ioutil.ReadAll(r)
-	//if err != nil {
-	//	return err
-	//}
-	//if SupportsWebP(headers) {
-		w.Header().Set("Content-Type", "image/webp")
-		///options := webp.Options{
-		//	Lossless: false,
-		//	Quality: 10,
-		//}
-		converter  := giftowebp.NewConverter()
-		converter.WebPConfig.SetEmulateJpegSize(1)
-		converter.WebPConfig.SetQuality(20)
-		converter.WebPConfig.SetPreprocessing(2)
-		converter.WebPConfig.SetThreadLevel(2)
-		converter.WebPAnimEncoderOptions.SetKmin(9)
-		converter.WebPAnimEncoderOptions.SetKmax(17)
-		webpBin, _  := converter.Convert(gifBin)
-		w.Write(webpBin)
-		//if err = webp.Encode(w, img, &options); err != nil {
-		//	return err
-		//}
-	//} //else {
-		//if err = gif.Encode(w, img, nil); err != nil {
-		//	return err
-		//}
-	//}
+	w.Header().Set("Content-Type", "image/webp")
+
+	converter  := giftowebp.NewConverter()
+	converter.WebPConfig.SetEmulateJpegSize(1)
+	converter.WebPConfig.SetQuality(20)
+	converter.WebPConfig.SetThreadLevel(2)
+	converter.WebPAnimEncoderOptions.SetKmin(9)
+	converter.WebPAnimEncoderOptions.SetKmax(17)
+	webpBin, _  := converter.Convert(gifBin)
+	w.Write(webpBin)
+
 	return nil
 }
