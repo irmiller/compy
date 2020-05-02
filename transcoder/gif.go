bcde3d3fee3ed37aa0958a9f2f37353d5313c5b2
irmiller <63781021+irmiller@users.noreply.github.com>
2020-05-02 09:00:45 +0200

    Update gif.go

--- transcoder/gif.go
+++ transcoder/gif.go
@@ -3,7 +3,7 @@ package transcoder
 import (
 	"github.com/irmiller/compy/proxy"
 	"github.com/chai2010/webp"
-	"image/gif"
+	//"image/gif"
 	"net/http"
 	"io/ioutil"
 	giftowebp "github.com/sizeofint/gif-to-webp"
@@ -14,9 +14,9 @@ type Gif struct{}
 func (t *Gif) Transcode(w *proxy.ResponseWriter, r *proxy.ResponseReader, headers http.Header) error {
 	//img, err := gif.Decode(r)
 	gifBin, _  := ioutil.ReadAll(r)
-	if err != nil {
-		return err
-	}
+	//if err != nil {
+	//	return err
+	//}
 	//if SupportsWebP(headers) {
 		w.Header().Set("Content-Type", "image/webp")
 		options := webp.Options{
