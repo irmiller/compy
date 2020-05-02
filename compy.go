9bb494a406fb9cce1c65221695e04ce660989ae7
irmiller <63781021+irmiller@users.noreply.github.com>
2020-05-02 22:39:26 +0200

    Update compy.go

--- compy.go
+++ compy.go
@@ -26,8 +26,8 @@ var (
 	gif    = flag.Bool("gif", true, "transcode gifs into static images")
 	gzip   = flag.Int("gzip", 6, "gzip compression level (0-9)")
 	png    = flag.Bool("png", true, "transcode png")
-	webm   = flag.Bool("webm", true, "transcode webm")
-	minify = flag.Bool("minify", false, "minify css/html/js - WARNING: tends to break the web")
+	webm   = flag.Bool("webm", false, "transcode webm")
+	minify = flag.Bool("minify", true, "minify css/html/js - WARNING: tends to break the web")
 )
 
 func main() {
