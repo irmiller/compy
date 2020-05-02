8a0a4252a53cbb18d76a81afe7cf7493d1e4c257
irmiller <63781021+irmiller@users.noreply.github.com>
2020-05-02 11:05:03 +0200

    Update webm.go

--- transcoder/webm.go
+++ transcoder/webm.go
@@ -12,14 +12,21 @@ import (
 type WebM struct{}
 
 func (t *WebM) Transcode(w *proxy.ResponseWriter, r *proxy.ResponseReader, headers http.Header) error {
-	webM, _  := ioutil.ReadAll(r)
-	var webMT []byte
 	w.Header().Set("Content-Type", "video/webm")
 	trans := new(transcoder.Transcoder)
-	err := trans.Initialize( webM, webMT )
+	err := trans.InitializeEmptyTranscoder()
+	
+	in, err := trans.CreateInputPipe()
+	webM = in.Read(ioutil.ReadAll(r))
+	
+	r, err := trans.CreateOutputPipe("webm")
+	
 	trans.MediaFile().SetPreset("ultrafast")
 	trans.MediaFile().SetQuality(20)
+	
 	done := trans.Run(false)
+	
+	webMT, err := ioutil.ReadAll(r)
 	
 	w.Write(webMT)
 	return nil
