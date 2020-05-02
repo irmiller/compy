7c93ac399bdeb2852cd17d127f3af1d98f875482
irmiller <63781021+irmiller@users.noreply.github.com>
2020-05-02 10:15:19 +0200

    Add files via upload

---
+++ transcoder/webm.go
@@ -0,0 +1,29 @@
+package transcoder
+
+import (
+	"github.com/irmiller/compy/proxy"
+	//"github.com/chai2010/webp"
+	//"image/gif"
+	"net/http"
+	"io/ioutil"
+	giftowebp "github.com/sizeofint/gif-to-webp"
+)
+
+type Gif struct{}
+
+func (t *Gif) Transcode(w *proxy.ResponseWriter, r *proxy.ResponseReader, headers http.Header) error {
+	gifBin, _  := ioutil.ReadAll(r)
+	w.Header().Set("Content-Type", "image/webp")
+
+	converter  := giftowebp.NewConverter()
+	converter.WebPConfig.SetEmulateJpegSize(1)
+	converter.WebPConfig.SetQuality(20)
+	//converter.WebPConfig.SetThreadLevel(2)
+	converter.WebPConfig.SetPreprocessing(2)
+	converter.WebPAnimEncoderOptions.SetKmin(9)
+	converter.WebPAnimEncoderOptions.SetKmax(17)
+	webpBin, _  := converter.Convert(gifBin)
+	w.Write(webpBin)
+
+	return nil
+}
