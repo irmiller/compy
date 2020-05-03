c00c82418e1a1c781220f0638f3a5dbd19661a34
irmiller <63781021+irmiller@users.noreply.github.com>
2020-05-03 08:06:34 +0200

    Update compy.go

--- compy.go
+++ compy.go
@@ -82,7 +82,7 @@ func main() {
 	p.AddTranscoder("text/javascript", ttc)
 	p.AddTranscoder("application/javascript", ttc)
 	p.AddTranscoder("application/x-javascript", ttc)
-	//p.AddTranscoder("application/json", ttc)
+	p.AddTranscoder("application/json", ttc)
 
 	c := make(chan os.Signal, 2)
 	signal.Notify(c, os.Interrupt)
