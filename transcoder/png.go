5956cb3e557cdfce59b864db9fe6242060944a21
irmiller <63781021+irmiller@users.noreply.github.com>
2020-05-12 22:48:11 +0200

    Update png.go

--- transcoder/png.go
+++ transcoder/png.go
@@ -10,29 +10,43 @@ import (
 // 	"log"
 	"fmt"
 	"io/ioutil"
-// 	giftowebp "github.com/sizeofint/gif-to-webp"
+	giftowebp "github.com/sizeofint/gif-to-webp"
 )
 
 type Png struct{}
 
 func (t *Png) Transcode(w *proxy.ResponseWriter, r *proxy.ResponseReader, headers http.Header) error {
-	img, err := ioutil.ReadAll(r)
+// 	img, err := ioutil.ReadAll(r)
 
-	// 	img, err := png.Decode(r)
+	img, err := png.Decode(r)
 // 	fmt.Println("Error 1 %s",err)
-// 	if err != nil {
-// 		err = nil
-// 		img, err = jpeg.Decode(r, &jpeg.DecoderOptions{})
-// 		fmt.Println("Error 2 %s",err)
-// 		if err != nil {
-// 			err = nil
-// 			img, err = ioutil.ReadAll(r)
-// 			fmt.Println("Error 3 %s",err)
-// 			if err != nil {
-// 				return err
-// 			}
-// 		}
-// 	}
+	if err != nil {
+		err = nil
+		img, err = jpeg.Decode(r, &jpeg.DecoderOptions{})
+		fmt.Println("Error 2 %s",err)
+		if err != nil {
+			err = nil
+			img, err := ioutil.ReadAll(r)
+			fmt.Println("Error 3 %s",err)
+			if err != nil {
+				return err
+			}
+			w.Header().Set("Content-Type", "image/webp")
+				options := webp.Options{
+				Lossless: false,
+				Quality: float32(25),
+			}
+			converter  := giftowebp.NewConverter()
+			converter.WebPConfig.SetQuality(25)
+			converter.WebPConfig.SetPreprocessing(2)
+			webpBin, _  := converter.Convert(gifBin)
+			if err = webp.Encode(w, webpBin, &options); err != nil {
+				return err
+			}
+			return nil
+			
+		}
+	}
 	if SupportsWebP(headers) {
 		w.Header().Set("Content-Type", "image/webp")
 		options := webp.Options{
