87ff7cfe608ee10ea6461414dd63a8b60a860d55
irmiller <63781021+irmiller@users.noreply.github.com>
2020-05-14 06:51:23 +0200

    Create encode.go

---
+++ transcoder/encode.go
@@ -0,0 +1,64 @@
+package transcoder
+
+import (
+	"github.com/irmiller/compy/proxy"
+	"github.com/chai2010/webp"
+	"github.com/pixiv/go-libjpeg/jpeg"
+	"net/http"
+	"image/png"
+	"strconv"
+	"io/ioutil"
+)
+
+type Jpeg struct {
+	decOptions *jpeg.DecoderOptions
+	encOptions *jpeg.EncoderOptions
+}
+
+func NewJpeg(quality int) *Jpeg {
+	return &Jpeg{
+		decOptions: &jpeg.DecoderOptions{},
+		encOptions: &jpeg.EncoderOptions{
+			Quality:        quality,
+			OptimizeCoding: true,
+		},
+	}
+}
+
+func (t *Jpeg) Transcode(w *proxy.ResponseWriter, r *proxy.ResponseReader, headers http.Header) error {
+	img, err := jpeg.Decode(r, t.decOptions)
+	if err != nil {
+		err = nil
+		img, err = png.Decode(r)
+		if err != nil {
+			img, _ := ioutil.ReadAll(r)
+			w.Write(img)
+			return nil
+		}
+	}
+	
+	encOptions := t.encOptions
+	qualityString := headers.Get("X-Compy-Quality")
+	if qualityString != "" {
+		if quality, err := strconv.Atoi(qualityString); err != nil {
+			return err
+		} else {
+			encOptions.Quality = quality
+		}
+	}
+	if SupportsWebP(headers) {
+		w.Header().Set("Content-Type", "image/webp")
+		options := webp.Options{
+			Lossless: false,
+			Quality:  float32(25),
+		}
+		if err = webp.Encode(w, img, &options); err != nil {
+			return err
+		}
+	} else {
+		if err = jpeg.Encode(w, img, encOptions); err != nil {
+			return err
+		}
+	}
+	return nil
+}
