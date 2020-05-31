a2e010588ce9f4a5ecfe9e28d548cd3284056566
irmiller <63781021+irmiller@users.noreply.github.com>
2020-05-31 14:00:00 +0200

    Update compy.go

--- compy.go
+++ compy.go
@@ -16,8 +16,8 @@ var (
 	host  = flag.String("host", ":8888", "<host:port>")
 	cert  = flag.String("cert", "", "proxy cert path")
 	key   = flag.String("key", "", "proxy cert key path")
-	ca    = flag.String("ca", "/home/irmillering/go/bin/cert.crt", "CA path")
-	caKey = flag.String("cakey", "/home/irmillering/go/bin/cert.key", "CA key path")
+	ca    = flag.String("ca", "/home/ubuntu/go/bin/ca.crt", "CA path")
+	caKey = flag.String("cakey", "/home/ubuntu/go/bin/ca.key", "CA key path")
 	user  = flag.String("user", "", "proxy user name")
 	pass  = flag.String("pass", "", "proxy password")
 
